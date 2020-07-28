dbc8cdfad9c3623d5c46005f0c5f603582d7c9d8
Harald Frebort <ce-harald.frebort@bearingpoint.com>
2020-07-28 12:00:48 +0200

    add some samples to show angularJS

--- billing-frontend/src/main/java/at/frebort/billing/frontend/CustomerController.java
+++ billing-frontend/src/main/java/at/frebort/billing/frontend/CustomerController.java
@@ -4,6 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +28,25 @@ public class CustomerController {
     @ResponseBody
     public List<Customer> getCustomers() {
         return this.repository.findAll();
+    }
+
+    @GetMapping("/customer/{customerId}")
+    @ResponseBody
+    public Customer getCustomer(@PathVariable("customerId") final int id) {
+        return this.repository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("customer does not exist!"));
+    }
+
+    @PostMapping("/customer")
+    public void insertCustomer(@RequestBody final Customer customer) {
+        if (this.repository.existsById(customer.getCustomerId())) {
+            throw new IllegalArgumentException("customer already exist!");
+        }
+        this.repository.save(customer);
+    }
+
+    @PutMapping("/customer")
+    public void updateCustomer(@RequestBody final Customer customer) {
+        this.repository.save(customer);
     }
 }
--- billing-frontend/src/main/webapp/consume.js
+++ billing-frontend/src/main/webapp/consume.js
@@ -3,11 +3,34 @@ angular.module('billing', [])
 	
 	console.log("initialize controller");
 	$scope.customers = [{"customerId":3,"firstName":"Paul","lastName":"Kafka"},{"customerId":4,"firstName":"Harald","lastName":"Frebort"}];
+	// $scope.customer = {"customerId":3,"firstName":"Paul","lastName":"Kafka"};
 	
-	/**
-    $http.get('http://localhost:8080/customers').then(function(response) {
+	$http.get('http://localhost:8080/customer/' + 3).then(function(response) {
     	console.log("done: ", response);
-        //$scope.customers = response.data;
-    });
-    */
+        $scope.customer = response.data;
+	});
+	
+	
+	this.insert = function() {
+		console.log("Das ist der customer: ", $scope.customer);
+		
+		$http.post('http://localhost:8080/customer', $scope.customer).then(function(response) {
+	    	console.log("saved: ", response);
+	    });
+		
+	};
+	
+	this.update = function() {
+		$http.put('http://localhost:8080/customer', $scope.customer).then(function(response) {
+	    	console.log("saved: ", response);
+	    });
+		
+	};
+
+	this.getAll = function() { 
+		$http.get('http://localhost:8080/customers').then(function(response) {
+	    	console.log("done: ", response);
+	        $scope.customers = response.data;
+		});
+	};
 });
